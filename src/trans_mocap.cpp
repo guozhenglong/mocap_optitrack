a5c3860adc47879fd5638ba9c44472fdfeae896d
guozhenglong <guozhenglong.cn@gmail.com>
2018-07-13 22:48:58 +0800

    failed to add distort in fake gps data.

--- src/trans_mocap.cpp
+++ src/trans_mocap.cpp
@@ -13,6 +13,7 @@ geometry_msgs::PoseStamped pos_mocap;
 
 void Pose_Mocap_CallBack(const geometry_msgs::PoseStamped::ConstPtr msg);
 ros::Publisher  fake_gps_pub;
+ros::Publisher mocap_pose_pub;
 int main(int argc, char* argv[])
 {
     ros::init(argc, argv, "mocap_fake_gps_node");
@@ -20,7 +21,8 @@ int main(int argc, char* argv[])
     ros::NodeHandle pnh("~");
 
     ros::Subscriber mocap_pos_sub = nh.subscribe("/Robot/pose",100, Pose_Mocap_CallBack);
-    fake_gps_pub = nh.advertise<geometry_msgs::TransformStamped>("/mocap_fake_gps",100);
+    fake_gps_pub = nh.advertise<geometry_msgs::TransformStamped>("/mocap_fake_gps",1000);
+    mocap_pose_pub = nh.advertise<geometry_msgs::PoseStamped>("/mocap_pose", 1000);
     ros::spin();
     return 0;
 }
@@ -28,6 +30,7 @@ int main(int argc, char* argv[])
 
 void Pose_Mocap_CallBack(const geometry_msgs::PoseStamped::ConstPtr msg)
 {
+    
     tf_fake_gps.header.frame_id = "/map";
     tf_fake_gps.header.stamp=ros::Time::now();
     tf_fake_gps.child_frame_id = "/mocap/intel_rtf";
@@ -39,6 +42,12 @@ void Pose_Mocap_CallBack(const geometry_msgs::PoseStamped::ConstPtr msg)
     tf_fake_gps.transform.rotation.x = msg->pose.orientation.x;
     tf_fake_gps.transform.rotation.y = msg->pose.orientation.y;
     tf_fake_gps.transform.rotation.z = msg->pose.orientation.z;
+
+    pos_mocap.header.frame_id = "/map";
+    pos_mocap.header.stamp = tf_fake_gps.header.stamp;
+    pos_mocap.pose = msg->pose;
+
     fake_gps_pub.publish(tf_fake_gps);
+    mocap_pose_pub.publish(pos_mocap);
     // cout<<"GPS=:"<<endl<<tf_fake_gps<<endl; 
 }
